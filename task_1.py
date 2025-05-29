8640775ab71e2a8dfd2ee98f009a1927866bd743
owykes <owenwykes1992@gmail.com>
2025-05-29 12:41:25 +0100

    task 1 seperate

--- task_1.py
+++ task_1.py
@@ -8,62 +8,28 @@ def wait_for_enter():
         if input("\nPress Enter to return to the menu...") == "":
             break
 
-#hardcoded property detials from table 1 in dictionary
-property_details = {
-    "B12-3AB": {"Original cost": 153450, "Residual mortgage": 112345, "Entries": []},
-    "B13-4CD": {"Original cost": 212130, "Residual mortgage": 180234, "Entries": []},
-    "B14-5GH": {"Original cost": 120100, "Residual mortgage": 85980, "Entries": []},
-    "B15-6JK": {"Original cost": 135230, "Residual mortgage": 101321, "Entries": []},
-    "B16-7MO": {"Original cost": 183230, "Residual mortgage": 130234, "Entries": []}
-}
-
-#asks for input, validates property id key, removing whitespace, handles incorrect capatilisation, avoids key errors
+#placeholder subroutine for property data
 def property_data():
     clear_screen()
-    while True:
-        property_id = input("Enter Property#: ").strip().upper()
-        property_info = property_details.get(property_id) #safelookup 
-        if property_info:
-            print("\nProperty found")
-            clear_screen()
-            break
-        else:
-            print("Please Enter a Valid Property#")
-    #ask for user input for description dictionary 
-    description = input("Enter Entry Description: ").strip() 
-    #asks for value input, and handles value errors
-    while True:
-        try:
-            amount = float(input("\nEnter Amount: "))
-            break
-        except ValueError:
-            print("\nPlease enter a valid amount")    
-    #appends the new to the dictionarys into the new list
-    property_info["Entries"].append({"Description": description, "Amount": amount})
-    print("Entry added")
-    print(property_details) #testprint
+    print("Enter Property Details")
     wait_for_enter()
         
-    
-
-#view stored data in list
+#placeholder subroutine for summary data
 def summary_data():
-    clear_screen() 
-    print()
-    #placeholder
-    print("placeholder")   
+    clear_screen()
+    print("Property Summary")  
     wait_for_enter()
-
+ 
 #displays menu and handles user selection
 def rental_management_menu():
-    while True:
+    while True:   
         clear_screen()
-        print("Rental Managment Menu")
+        print("Rental Management Menu")
         print("1. Enter rental property details")
         print("2. Display summary for rentals")
         print("3. Exit")
         
-        #maybe remove the prompt from this input
+        #prompts user for selection
         selection = input("\nEnter your Selection: ")
 
         if selection == "1":
@@ -76,6 +42,7 @@ def rental_management_menu():
         else:
             print("Invalid choice, please select 1,2 or 3")
             wait_for_enter()
+           
            
 rental_management_menu()
 
