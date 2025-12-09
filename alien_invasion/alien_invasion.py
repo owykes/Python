2aa2def4e29d5e970bf825d9ea218eb3155a52df
owykes <owenwykes1992@gmail.com>
2025-12-09 22:31:31 +0000

    ship movement

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -23,19 +23,37 @@ class AlienInvasion:
     def run_game(self):
         """start main loop for the game"""
         while True:
-            #watch for mouse and keyboard events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)                      
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):   
+        """update images om the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #make the most recently drawn screen visible
+        pygame.display.flip()
             
-            #redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            #make the most recently drawn screen visible
-            pygame.display.flip()
-            self.clock.tick(60)
-
+            
 if __name__ == '__main__':
     #make a game instance, and run the game.
     ai = AlienInvasion()
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -8,11 +8,22 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         #load a ship image and get its rect.
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load("alien_invasion/images/ship.bmp")
         self.rect = self.image.get_rect()
 
         #start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        #Movement flags : start with a ship that isn't moving
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ships position based on the movement flag"""
+        if self.moving_right:
+            self.rect.x += 2
+        if self.moving_left:
+            self.rect.x -= 2
+
 
     def blitme(self):
         """Draw the ship at its current location"""
