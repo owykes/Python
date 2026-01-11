10072de2d8386ef6fb8f233cb733fc698e19fb4c
owykes <owenwykes1992@gmail.com>
2026-01-11 00:57:04 +0000

    completed bubble sort , starting insertion

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,13 @@
+from helper import generate_list
+
+def insertion_sort():
+    """"""
+
+
+
+
+if __name__ == "__main__":
+    # update argument to change n
+    my_list = generate_list()
+    result = insertion_sort(my_list)
+    
