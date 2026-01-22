7742991ca0d288f6448f536a480b4b4af4e25a78
owykes <owenwykes1992@gmail.com>
2026-01-22 21:40:54 +0000

    new algorithms

--- algorithms/merge_sort.py
+++ algorithms/merge_sort.py
@@ -1,12 +1,42 @@
 from helper import generate_list
 
 def merge_sort(arr):
-    """ Splits the list into two halves, sorts each half recursively, merges the two sorted halves
+    """ Splits the list into two halves, sorts each half recursively, 
     best case: O(nlogn) worst case: O(nlogn) """
 
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
 
+    sorted_left = merge_sort(left)
+    sorted_right = merge_sort(right)
 
+    return merge(sorted_left, sorted_right)
 
+def merge(left, right):
+    """merges the two sorted halves"""
+    result = []
+
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+    
 if __name__ == "__main__":
     my_list = generate_list()
+    print(my_list)
     print(merge_sort(my_list))
