8bfdae2005fdaf89d1cf9d0b035cdc9e47ac8bff
owykes <owenwykes1992@gmail.com>
2026-01-29 22:40:37 +0000

    data vis , random walk vis, refactor

--- data_visualization/random_walk.py
+++ data_visualization/random_walk.py
@@ -8,11 +8,31 @@ class RandomWalk:
         self.num_points = num_points
 
         # All walks start at (0, 0).
-        self.x_values = [0]
-        self.y_values = [0]
+        self.x_values = [0, 0]
+        self.y_values = [0, 0]
 
     def fill_walk(self):
-        """Calculate all the ponits in the walk. """
+        """Calculate all the points in the walk. """
         # Keep taking steps until the walk reaches the desired length length.
         while len(self.x_values) < self.num_points:
-             # 
+             
+            x_step = self.get_step()
+            y_step = self.get_step()
+
+            # Reject moves that go nowhere
+            if x_step == 0 and y_step == 0:
+                continue
+
+            # Calcutlate the new position
+            x = self.x_values[-1] + x_step
+            y = self.y_values[-1] + y_step
+            
+            self.x_values.append(x)
+            self.y_values.append(y)
+
+    def get_step(self):
+        """Determine the direction and distance for each step"""
+         # Decide which direction to go, and how far to go.
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4,])
+        return direction * distance
