c1b34b5b7b02637094d9ba2ca3395e2fb9b23076
owykes <owenwykes1992@gmail.com>
2026-02-13 18:51:50 +0000

    plotly api request, hover text

--- data_visualization/working_with_apis/python_repos.py
+++ data_visualization/working_with_apis/python_repos.py
@@ -10,6 +10,21 @@ print(f"Status code: {r.status_code}")
 
 # Convert the response object to a dictionary
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
 
-# Process results.
-print(response_dict.keys())
+# Explore infromation about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+
+
