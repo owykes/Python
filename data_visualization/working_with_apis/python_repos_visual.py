13186bc588887bbfa5104f6cadab92c13e84becc
owykes <owenwykes1992@gmail.com>
2026-02-16 19:12:09 +0000

    django project started

--- data_visualization/working_with_apis/python_repos_visual.py
+++ data_visualization/working_with_apis/python_repos_visual.py
@@ -15,11 +15,15 @@ print(f"Complete results: {not response_dict['incomplete_results']}")
 
 # Process repository infromation.
 repo_dicts = response_dict['items']
-repo_names, stars, hover_texts = [], [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    #turn repo names into active links.
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
 
+    stars.append(repo_dict['stargazers_count'])
     # Build hover texts.
     owner = repo_dict['owner']['login']
     description = repo_dict['description']
@@ -27,8 +31,9 @@ for repo_dict in repo_dicts:
     hover_texts.append(hover_text)
 
 # Make a visualization
-title = "Most-Starred Python Projects onm GitHub"
+title = "Most-Starred Python Projects on GitHub"
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels, hover_name=hover_texts)
+fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hover_texts)
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
+fig.update_traces(marker_color='SteelBlue',  marker_opacity=0.6)
 fig.show()
