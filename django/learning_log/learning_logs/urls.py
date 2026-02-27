b741567d9e3ad552b34e7299809344d6f678da90
owykes <owenwykes1992@gmail.com>
2026-02-27 14:52:01 +0000

    new topic template, updated url and models

--- django/learning_log/learning_logs/urls.py
+++ django/learning_log/learning_logs/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     # Page that shows all topics.
     path('topics/', views.topics, name='topics'), 
     # Detail page for a single topic.
-    path('topics/<int:topic_id>/', views.topic, name='topic')
-    
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
 
